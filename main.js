f7065e849daa1f783d5af0d9f09f4eb4f231ad6d
Tntrum <010francisjacob@gmail.com>
2024-05-13 12:25:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const input = document.querySelector('input')
 const button = document.querySelector('button')
 
 button.addEventListener('click', ()=>{
-  window.open('page2.html', '_self')
+  
+  if (input.value == '72TMS8071972BGJ') {
+    window.open('page2.html', '_self')
+  } else {
+    alert('Invalid, tracking number must be a combination of alphanumeric characters of 5-50 characters in length.')
+  }
+  
 })
